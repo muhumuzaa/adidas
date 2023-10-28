1238b279d5c1e3e78f346f898d6fd29c8d8d909d
Ambroze Muhumuza <142443950+muhumuzaa@users.noreply.github.com>
2023-10-28 20:14:57 +0000

    fst

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+import './App.css'
+
+const App = () =>(
+  <main className='relative'>
+   <nav>
+    Nav
+   </nav>
+   Hero
+   <section className='xl: padding-1 wide:padding-r padding-b'>
+    Hero
+   </section>
+
+   <section className='padding'>PopularProducts</section>
+
+   <section className='padding'>SuperQuality</section>
+
+   <section className='padding-x py-10'>Services</section>
+
+   <section className='padding'>SpecialOffer</section>
+
+   <section className='padding bg-pale-blue'>CustomerReviews</section>
+
+   <section className='padding-x sm: py-32 py-16 w-full'>Subscribe</section>
+
+   <section className='padding bg-black padding-x padding-t pb-8'>Footer</section>
+  </main>
+)
+
+export default App
