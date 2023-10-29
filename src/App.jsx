ba49918102323cee6360cdc08d3d6dc7d060333d
Ambroze Muhumuza <142443950+muhumuzaa@users.noreply.github.com>
2023-10-29 03:26:00 +0000

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+
 
 import './App.css'
-import {Nav} from './components/Nav'
+
+import Nav from "./components/Nav"
 
 import {Hero,
   PopularProducts,
@@ -16,26 +17,29 @@ import {Hero,
 const App = () =>{
   
   return (
+  
   <main className='relative'>
-   <Nav />
+  <Nav /> 
    <section className='xl: padding-1 wide:padding-r padding-b'>
-   < Hero />
+   <Hero /> 
    </section>
 
-   <section className='padding'> <PopularProducts /> </section>
+   <section className='padding'> <PopularProducts />  </section>
 
    <section className='padding'> <SuperQuality /> </section>
 
-   <section className='padding-x py-10'> <Services /> </section>
+   <section className='padding-x py-10'> <Services /></section>
 
    <section className='padding'> <SpecialOffer /> </section>
 
-   <section className='padding bg-pale-blue'> <CustomerReviews /> </section>
+   <section className='padding bg-pale-blue'><CustomerReviews />  </section>
 
-   <section className='padding-x sm: py-32 py-16 w-full'> <Subscribe /> </section>
+   <section className='padding-x sm: py-32 py-16 w-full'> <Subscribe />  </section>
 
-   <section className='padding bg-black padding-x padding-t pb-8'> <Footer /> </section>
+   <section className='padding bg-black padding-x padding-t pb-8'> <Footer />  </section>
   </main>
+ 
 )}
 
 export default App
+
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import {arrowRight}  from '../assets/icons'
+import Button from '../components/Button'
+import { statistics } from '../constants'
 
 const Hero = () => {
   return (
-    <div>Herrrrrooo</div>
+    <section className='w-full border-2 border-red-500 py-2 min-h-screen flex xl:flex-row flex-col justify-center gap-10 max-container'>
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28'>
+        <p className='text-xl font-montserrat text-coral-red'>Our New Collection</p>
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold'>
+          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>The New Arrival</span>
+          <br />
+          <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
+        </h1>
+        <p>Single distinct meaningful element of speech or writing, used with others (or sometimes alone) to form a sentence and typically shown with a space on either side when written or printed.</p>
+        <Button label = "Shop Now" iconURL ={arrowRight}/>
+
+        <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
+          {statistics.map((stat) =>(
+            <div key={stat.label}>
+              <p>{stat.value}</p>
+              <p>{stat.label}</p>
+            </div>
+          ))}
+        </div>
+
+
+      </div>
+    </section>
   )
 }
 
